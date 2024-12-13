a824ba786b457c44a4405ad466f52a3c505502c5
AdnaneAbou <adnan.aboutalib2014@gmail.com>
2024-12-14 00:27:47 +0100

    Pattern 8 : Overlapping Intervals , Merge Intervals

---
+++ 36_Merge_Intervals/solution.py
@@ -0,0 +1,23 @@
+def merge(intervals):
+    """
+    :type intervals: List[List[int]]
+    :rtype: List[List[int]]
+    """
+
+    if len(intervals) == 1:
+        return intervals
+    
+    intervals = sorted(intervals, key=lambda x : x[0])
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][-1] < interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][-1] = max(merged[-1][-1],interval[1])
+
+    return merged
+
+
+
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+print(merge(intervals))
