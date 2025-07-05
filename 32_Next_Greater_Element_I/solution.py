68f937587d26f9752a4db6e6b2d79ffe32409d02
AdnaneAbou <adnan.aboutalib2014@gmail.com>
2025-07-05 10:04:27 +0100

    Monotonic stack Left to right

--- 32_Next_Greater_Element_I/solution.py
+++ 32_Next_Greater_Element_I/solution.py
@@ -19,6 +19,34 @@ def nextGreaterElement(nums1, nums2):
     return res
 
 
+def nextGreaterElement_(nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    Traversing the array from left to right
+    """
+
+    result = [-1]* len(nums1)
+    stack = []
+    nums_to_id = {n:i for i,n in enumerate(nums1)}
+    
+    
+    for i in range(len(nums2)-1,-1,-1):
+        
+        while stack and stack[-1] <= nums2[i]:
+            stack.pop()
+        
+        if stack and nums2[i] in nums_to_id.keys():
+            idx = nums_to_id[nums2[i]]
+            result[idx] = stack[-1]
+
+        stack.append(nums2[i])
+    
+
+    return result
+
+
 nums1 = [4,1,2]
 nums2 = [1,3,4,2]
 
